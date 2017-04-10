5a0b25718a10d71083bb66536a1f56986688a5e7
Lemmah <lemayiannakolah@gmail.com>
2017-04-11 01:49:33 +0300

    Rethinking test cases for the class person"

--- tests/test_class_person.py
+++ tests/test_class_person.py
@@ -8,11 +8,18 @@ from scripts.class_person import *
 class TestClassPerson(unittest.TestCase):
     # test1: check whether the default value of a person's room status is
     # unallocated.
-    # test2: check that class person returns person's name,
-    # room_allocation_status
+    # test2: should be able to get full names when both names are provided
+    # test3: add new person returns the correct statement
 
     def test_room_allocation_status(self):
-        self.assertTrue(ClassPerson('James').room_allocation_status ==
-                        False, msg='Person should by default have no room.')  # test1
-        self.assertEqual(print(ClassPerson('James')), 'James, False',
-                         msg='Return value of Person Class should be name, room_allocation_status')  # test2
+        self.assertTrue(Person('James').office_space is
+                        None, 'Person should by default have no room.')  # test1
+
+    def test_full_name_generation(self):
+        self.assertEqual(Person('Fellow', 'James', 'Lemayian').full_name(),
+                         'James Lemayian',
+                         'The full name should be a conc of first and second name!')  # test2
+
+    def test_add_new_person(self):
+        self.assertEqual(Person('Fellow', 'Joshua', 'Ondieki').new_person(
+        ), 'Fellow Joshua Ondieki has been successfully added.')  # test3
