b702608a2b4b5abc6e4c04bbf9de2b697832cc2d
Lemmah <lemayiannakolah@gmail.com>
2017-04-10 17:02:56 +0300

    Tests for class person.

--- tests/test_class_person.py
+++ tests/test_class_person.py
@@ -6,4 +6,13 @@ from scripts.class_person import *
 
 
 class TestClassPerson(unittest.TestCase):
-    pass
+    # test1: check whether the default value of a person's room status is
+    # unallocated.
+    # test2: check that class person returns person's name,
+    # room_allocation_status
+
+    def test_room_allocation_status(self):
+        self.assertTrue(ClassPerson('James').room_allocation_status ==
+                        False, msg='Person should by default have no room.')  # test1
+        self.assertEqual(print(ClassPerson('James')), 'James, False',
+                         msg='Return value of Person Class should be name, room_allocation_status')  # test2
