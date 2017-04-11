4141674780071d08a90e1595783744069ec46a70
Lemmah <lemayiannakolah@gmail.com>
2017-04-11 22:36:21 +0300

    Redifining Dojo Class

--- scripts/class_dojo.py
+++ scripts/class_dojo.py
@@ -1,2 +1,10 @@
+"""
+
+This is the master of this game. The control class so to call it.
+It calls instances of other classes and
+mainly interacts with the docopt user interface.
+
+"""
+
 class Dojo:
     pass
