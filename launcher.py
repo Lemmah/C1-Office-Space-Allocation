6e1d31b6909ac0fd86141fc64810db1b7416d63f
Lemmah <lemayiannakolah@gmail.com>
2017-04-11 14:48:15 +0300

    Updated the docopt interface.

--- launcher.py
+++ launcher.py
@@ -1,14 +1,17 @@
 """
-This example uses docopt with the built in cmd module to demonstrate an
-interactive command application.
+
+This commandline interface uses docopt with the built in cmd module.
+
 Usage:
-    note add_note <note>
-    note (-i | --interactive)
-    note (-h | --help)
+    mra add_person <first_name> <last_name> <person_type> [<wants_accomodation>]
+    mra create_room <room_type> <room_name>
+    mra (-i | --interactive)
+    mra (-h | --help)
 Options:
     -o, --output  Save to a txt file
     -i, --interactive  Interactive Mode
     -h, --help  Show this screen and exit.
+
 """
 
 import sys
@@ -36,7 +39,7 @@ def docopt_cmd(func):
             # The DocoptExit is thrown when the args do not match.
             # We print a message to the user and the usage block.
 
-            print('Invalid Command!')
+            print('Invalid command usage!')
             print(e)
             return
 
@@ -56,18 +59,53 @@ def docopt_cmd(func):
 
 class MyInteractive (cmd.Cmd):
 
-    prompt = '>>> '
+    prompt = 'mra >>> '
     file = None
 
     @docopt_cmd
-    def do_add_note(self, args):
-        """Usage: add_note <note>"""
+    def do_add_person(self, args):
+        """Usage: add_person <first_name> <last_name> <person_type> [<wants_accomodation>]"""
+        first_name = args['<first_name>']
+        last_name = args['<last_name>']
+        person_type = args['<person_type>']
+        wants_accomodation = args['<wants_accomodation>']
 
-        print(noteTaking().take_note(args))
+        if person_type.upper() == 'FELLOW' or person_type.upper() == 'STAFF':
+            print('\n' + '*' * 25 + ' Success ' + '*' * 26)
+            print(Person(person_type, first_name, last_name).new_person())
+            print(Person(person_type, first_name, last_name).allocate_office())
+        else:
+            print('\n' + '*' * 26 + ' ERROR! ' + '*' * 26)
+            print(
+                'At the moment, we\'re working with either staff or fellows.\nPlease input a valid person_type.')
+        if wants_accomodation and person_type.upper() == 'FELLOW':
+            print(Fellow(
+                first_name, last_name,
+                person_type, wants_accomodation).allocate_living_space())
+        elif wants_accomodation and person_type.upper() == 'STAFF':
+            print('\n' + '*' * 26 + ' ERROR! ' + '*' * 26)
+            print(
+                'OOPSIE! Accomodation is available to Fellows only.')
+        print('*' * 60)
+
+    @docopt_cmd
+    def do_create_room(self, args):
+        """Usage: create_room <room_type> <room_name>..."""
+        room_type = args['<room_type>']
+        print('\n' + '*' * 25 + ' Success ' + '*' * 26)
+        for item in args['<room_name>']:
+            room_name = item
+            if room_type.upper() == 'LIVINGSPACE' or room_type.upper() == 'OFFICE':
+                print(Room(room_type, room_name).add_new_room()[0])
+            else:
+                print('\n' + '*' * 26 + ' ERROR! ' + '*' * 26)
+                print('<room_type> type must either be Office or LivingSpace')
+
+        print('\n' + '*' * 60)
 
     def do_quit(self, args):
         """Quits out of Interactive Mode."""
-        print('Hope I made your work easier. Bye!')
+        print('\n\tHope I made your work easier. Bye!\n')
         exit()
 
 
