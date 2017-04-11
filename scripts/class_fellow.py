9bc5dfaa87a274fe89bb8dab33e59c52f329a1db
Lemmah <lemayiannakolah@gmail.com>
2017-04-11 14:41:38 +0300

    Class Fellow that inherits from Person

--- scripts/class_fellow.py
+++ scripts/class_fellow.py
@@ -1,5 +1,31 @@
 from scripts.class_person import *
+'''
+
+This fellow class inherits from Person.
+Simple, it only enables fellows get living spaces
+since staff should not have living spaces.
+
+'''
 
 
 class Fellow(Person):
-    pass
+    def __init__(
+            self, first_name,
+            second_name, person_type,
+            living_space=None):
+
+        super().__init__(first_name, second_name, person_type)
+        self.first_name = first_name
+        self.second_name = second_name
+        self.person_type = person_type
+        self.living_space = living_space
+        self.availble_living_spaces = ['Python', 'Ruby', 'JavaScript', 'Java']
+        self.allocated_living_space = random.choice(
+            self.availble_living_spaces)
+
+    def allocate_living_space(self):
+        if self.living_space is None:
+            pass
+        else:
+            return '{} has been allocated the livingplace {}.'.format(
+                self.first_name, self.allocated_living_space)
