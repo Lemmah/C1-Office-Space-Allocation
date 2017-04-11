9403562de17c1a4560c35d995e2fbd4f7f17e99b
Lemmah <lemayiannakolah@gmail.com>
2017-04-11 14:49:17 +0300

    Living space inheriting methods from Room

--- scripts/class_living_space.py
+++ scripts/class_living_space.py
@@ -1,5 +1,8 @@
-from scripts.class_dojo import *
+from scripts.class_room import *
 
 
-class LivingSpace(Dojo):
-    pass
+class LivingSpace(Room):
+    def __init__(self, room_type, room_name):
+        super().__init__(room_type, room_name)
+        self.room_name = room_name
+        self.room_type = room_type
