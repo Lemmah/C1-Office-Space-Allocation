4d84b8ed9b2dba5dffa436df5622418c2514f1de
Lemmah <lemayiannakolah@gmail.com>
2017-04-12 16:16:23 +0300

    Defining tests for version 0.

--- tests/test_class_dojo.py
+++ tests/test_class_dojo.py
@@ -17,3 +17,14 @@ class TestClassDojo(unittest.TestCase):
         self.assertTrue(blue_office)
         new_room_count = len(room_class_instance.all_rooms)
         self.assertEqual(new_room_count - initial_room_count, 1)
+
+    def test_reject_existing_room(self):
+        pass
+
+    # test1: test if a person is added to the list of people succesfully
+    # test2: test if a person is rejected if he already exists
+    def test_add_person_successfully(self):
+        pass
+
+    def test_reject_existing_person(self):
+        pass
