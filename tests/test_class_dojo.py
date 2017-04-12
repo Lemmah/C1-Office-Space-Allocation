d8e241352f4f75ee86d9e8c13ac4c32c2ae81739
Lemmah <lemayiannakolah@gmail.com>
2017-04-12 14:04:32 +0300

    Creating test for creating room functionality.

--- tests/test_class_dojo.py
+++ tests/test_class_dojo.py
@@ -7,4 +7,13 @@ from scripts.class_dojo import Dojo
 
 
 class TestClassDojo(unittest.TestCase):
-    pass
+    # test1: test if new room is appended to list (well done in the example)
+    # test2: reject room addition if name already exists...
+
+    def test_create_room_successfully(self):  # test1: done in example
+        room_class_instance = Dojo()
+        initial_room_count = len(room_class_instance.all_rooms)
+        blue_office = room_class_instance.create_room(“Blue”, “office”)
+        self.assertTrue(blue_office)
+        new_room_count = len(room_class_instance.all_rooms)
+        self.assertEqual(new_room_count - initial_room_count, 1)
